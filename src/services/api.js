7e321f57fd9d919db5474fa362eaca8d5e2ceaab
Sruthi_06s <23eg105r28@anurag.edu.in>
2026-03-14 22:01:22 +0530

    commit api

--- src/services/api.js
+++ src/services/api.js
@@ -1,24 +1,21 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:9090';
+const API_BASE_URL = 'https://kalakart-backend-4i2k.onrender.com';
+
+// Basic Auth credentials (from your backend logs)
+const username = 'user';
+const password = '5f637b95-9714-4d36-a5d1-e717542455f6';
+const basicAuth = 'Basic ' + btoa(username + ':' + password);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+    'Authorization': basicAuth  // Always send Basic Auth
   },
 });
 
-// Add token to requests if available
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// You can remove the token interceptor since we're using Basic Auth
+// Or keep it if you'll switch to JWT later
 
 export default api;
